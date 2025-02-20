fadc98113e12259882c3c24472d5ee24c515ce67
mikhail-belii <sasama15555@gmail.com>
2025-02-20 18:39:56 +0700

    init commit

---
+++ BusinessLogic/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Common.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLogic;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<UserEntity> Users { get; set; }
+
+}
---
+++ Common/DbModels/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.DbModels;
+
+public class UserEntity(Guid id, DateTime createTime, string name, string email, string passwordHash)
+{
+    public Guid Id { get; set; } = id;
+    public DateTime CreateTime { get; set; } = createTime;
+    public string Name { get; set; } = name;
+    public string Email { get; set; } = email;
+    public string PasswordHash { get; set; } = passwordHash;
+}
