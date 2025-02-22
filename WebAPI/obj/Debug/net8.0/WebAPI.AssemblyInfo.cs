094512eb94d295c92253ff65dd5fef278e53b515
Faddellin <belovodchenko2005@mail.ru>
2025-02-22 22:15:19 +0700

    after merge commit

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d367eb8c1854e2a5b5650149a52e6a59f3edd8b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67b9473ef5e0b0471bb66c790eb8d1369bc65180")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
